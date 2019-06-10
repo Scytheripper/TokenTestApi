a2a3c690f1cf47a5be0b92e3d1486f9ca28c6e28
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2019-06-10 08:50:48 -0400

    added endpoint to return sample token

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/token', (req, res, next) => {
+  res.json(
+    {
+      "data":{
+        "webToken" : "THISISAWEBTOKEN"
+      }
+    }
+  );
+});
+module.exports = router;
